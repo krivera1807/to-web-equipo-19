22c223c61991244640c0481db9fda333e7f4a477
unknown <palmierifacundo@gmail.com>
2023-10-01 20:15:51 -0300

    Creación del proyecto e incorporación de Boostrap.

---
+++ tp-web-equipo-19/tp-web-equipo-19/Default.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="tp_web_equipo_19.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <H1> Funciona.. </H1>
+</asp:Content>
---
+++ tp-web-equipo-19/tp-web-equipo-19/Default.aspx.designer.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace tp_web_equipo_19
+{
+
+
+    public partial class Default
+    {
+    }
+}
