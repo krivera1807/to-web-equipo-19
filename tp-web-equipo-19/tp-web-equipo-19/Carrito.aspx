1825ac3874b8a315053c633d4f683906d0965204
unknown <palmierifacundo@gmail.com>
2023-10-07 12:46:04 -0300

    Agrego clase carrito y modifico función página carrito.

---
+++ tp-web-equipo-19/tp-web-equipo-19/Carrito.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="Carrito.aspx.cs" Inherits="tp_web_equipo_19.Carrito" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="container text-center">
+        <h3>LISTADO DE COMPRAS</h3>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>PRODUCTO</th>
+                    <th>DESCRIPCION</th>
+                    <th>PRECIO</th>
+                </tr>
+            </thead>
+            <tbody>
+                <asp:Repeater ID="RepeaterCarrito" runat="server">
+                    <ItemTemplate>
+                        <tr>
+                            <td><%# Eval("Nombre") %></td>
+                            <td><%# Eval("Descripcion") %></td>
+                            <td><%# Eval("Precio") %></td>
+                        </tr>
+                    </ItemTemplate>
+                </asp:Repeater>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<div>Total</div>
+
+
+</asp:Content>
---
+++ tp-web-equipo-19/tp-web-equipo-19/Carrito.aspx.cs
@@ -0,0 +1,46 @@
+﻿using Dominio;
+using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace tp_web_equipo_19
+{
+    public partial class Carrito : System.Web.UI.Page
+    {
+        public string user { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //user = Session["Id"].ToString() != null ? Session["Id"].ToString() : "";
+
+            if (Request.QueryString["Id"]!= null) 
+            {
+                int id = int.Parse(Request.QueryString["Id"].ToString());
+
+                List<Articulo> ListaArticulos;
+                ArticuloNegocio articulo = new ArticuloNegocio();
+                ListaArticulos = articulo.Listar();
+                Articulo seleccionado = ListaArticulos.Find(x => x.Id == id);
+
+                // Filtrar la lista para obtener solo el artículo seleccionado
+                List<Articulo> ListaSeleccionada = Session["Carrito"] as List<Articulo>; // Usa "Carrito" en lugar de "Listaseleccionada"
+
+                if (ListaSeleccionada == null)
+                {
+                    ListaSeleccionada = new List<Articulo>();
+                    Session["Carrito"] = ListaSeleccionada;
+                }
+
+                // Agrega el artículo seleccionado a la lista del carrito
+                ListaSeleccionada.Add(seleccionado);
+
+                // Enlazar el Repeater con la lista filtrada
+                RepeaterCarrito.DataSource = ListaSeleccionada;
+                RepeaterCarrito.DataBind();
+            }
+        }
+    }
+}
